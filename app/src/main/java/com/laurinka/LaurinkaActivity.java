6f49b77695d5e3584746e5986f4af69ce33b8781
Radim <radim.pavlicek@gmail.com>
2015-12-10 22:33:24 +0100

    wip intro screen, statistics

--- app/src/main/java/com/laurinka/LaurinkaActivity.java
+++ app/src/main/java/com/laurinka/LaurinkaActivity.java
@@ -3,8 +3,6 @@ package com.laurinka;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-
 
 import com.laurinka.android.golf.puttingzones.MainActivity;
 import com.laurinka.android.golf.puttingzones.R;
--- app/src/main/java/com/laurinka/android/golf/puttingzones/MainActivity.java
+++ app/src/main/java/com/laurinka/android/golf/puttingzones/MainActivity.java
@@ -1,13 +1,31 @@
 package com.laurinka.android.golf.puttingzones;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        prefs = getSharedPreferences("data", MODE_PRIVATE);
+    }
+
+    public void add(View view) {
+        //startActivity(new Intent(this, AddByActivity.class));
+    }
+
+    public void showStats(View view) {
+    }
+
+    public void close(View view) {
+        finish();
     }
 }
