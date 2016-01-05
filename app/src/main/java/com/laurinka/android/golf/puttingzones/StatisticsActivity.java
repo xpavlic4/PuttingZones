574076f4624facab691de37f26317a267a0ba87c
Radim Pavlicek <radim.pavlicek@gmail.com>
2016-01-05 16:07:25 +0100

    statistics page

---
+++ app/src/main/java/com/laurinka/android/golf/puttingzones/StatisticsActivity.java
@@ -0,0 +1,31 @@
+package com.laurinka.android.golf.puttingzones;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Shows statistics.
+ */
+public class StatisticsActivity extends Activity {
+    private SharedPreferences prefs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.statistics);
+        prefs = getSharedPreferences("data", MODE_PRIVATE);
+
+        fillTextView("0", R.id.textView0);
+        fillTextView("1", R.id.textView1);
+        fillTextView("2", R.id.textView2);
+        fillTextView("3", R.id.textView3);
+        fillTextView("4", R.id.textView4);
+        fillTextView("5", R.id.textView5);
+    }
+
+    private void fillTextView(String key, int textView0) {
+        ((TextView) findViewById(textView0)).setText(prefs.getString(key, "0"));
+    }
+}
